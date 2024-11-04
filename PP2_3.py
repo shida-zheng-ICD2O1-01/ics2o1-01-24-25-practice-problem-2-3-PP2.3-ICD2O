96ad7c454328b4a8bbaeff2e890e623c99e0a859
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-11-04 10:10:51 -0500

    Update PP2_3.py

--- PP2_3.py
+++ PP2_3.py
@@ -1,12 +1,24 @@
 
-
 def q1(): 
   #Write Assignment code here
-
+word = input("In: ")
+if word.endswith("ife"):
+    print("-ives")
+elif word.endswith("ey"):
+    print("-eys")
+elif word.endswith("y"):
+    print("-ies")
+else:
+    print("-s")
 
 def q2(): 
   #Write Assignment code here
+num = int(input("In: "))
 
+if num > 0:
+    print(f"{num} is positive")
+elif num < 0:
+    print(f"{num} is negative")
 
 
 #Do not alter the following code
